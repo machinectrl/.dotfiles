04af9c773f6110876aa227817af9710850d0e79b
nate <natebeaulieu@outlook.com>
2024-04-10 21:38:37 -0400

    tmux stuff

--- .config/nvim/lua/core/set.lua
+++ .config/nvim/lua/core/set.lua
@@ -1,11 +1,16 @@
 vim.opt.nu = true
+--vim.opt.relativenumber = true
+
 vim.opt.termguicolors = true
 vim.opt.wrap = false
+
 vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 vim.opt.scrolloff = 8
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
